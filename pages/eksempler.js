4b715f56228b8aa19069ff4043138f1b7d5b0b72
dzgreen <dominik2711@gmail.com>
2021-11-28 15:01:14 +0100

    added eksempler.js

--- pages/eksempler.js
+++ pages/eksempler.js
@@ -7,6 +7,20 @@ export default function eksempler() {
         <title>Sindri</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto pt-40">
+          {
+            <article class="prose lg:prose-xl">
+              <h1>Garlic bread with cheese: What the science tells us</h1>
+              <p>
+                For years parents have espoused the health benefits of eating garlic bread with cheese to their children, with the food earning such an iconic status in our culture that kids will often dress up as warm, cheesy loaf for Halloween.
+              </p>
+              <p>But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases springing up around the country.</p>
+            </article>
+          }
+        </div>
+      </div>
     </div>
   );
 }
---
+++ pages/franio.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function Franio() {
+  return (
+    <div className="bg-red-400 min-h-screen">
+      <p className="text-center md:text-4xl">Hejsa</p>
+    </div>
+  );
+}
