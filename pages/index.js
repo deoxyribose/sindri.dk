e47b370fb2579fd292127fae50d46fc1c27335ff
dzgreen <dominik2711@gmail.com>
2021-11-28 14:00:28 +0100

    commnetet out JIT to resolve images not showiing in prod

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center w-full flex-1 text-center px-4 space-y-10 text-gray-200 bg-gray-900 min-h-screen">
         <h1 className="text-4xl md:text-8xl font-bold">Sindri</h1>
         <h2 className="text-2xl md:text-3xl">
-          Hejsa. Vi laver forklarlig <span className="text-green-400">maskinlæring og data analyse</span>,
+          Vi laver forklarlig <span className="text-green-400">maskinlæring og data analyse</span>,
           <br /> sætter <span className="text-red-400">dataarkitektur</span> op i skyen, <br />
           og får det hele til at gå op i en højere enhed med <br />
           brugervenlige <span className="text-yellow-400">applikationer</span>.
