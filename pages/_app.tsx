6b09dcf17777d04db89a8561603b5162e03e6652
dzgreen <dominik2711@gmail.com>
2022-06-03 12:11:12 +0200

    reainstalled packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import "styles/global.css"
-import PlausibleProvider from "next-plausible"
 import type { AppProps } from "next/app"
 import { ThemeProvider } from "next-themes"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PlausibleProvider domain="sindri.dk">
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </PlausibleProvider>
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
